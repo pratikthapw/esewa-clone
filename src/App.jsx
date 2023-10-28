85e4b7f382d9759a0235afce32901bb613a2aa60
pratikthapw <pratikthapw@gmail.com>
2023-10-28 12:58:17 +0545

    bootstrap styles overriden, created & styled navBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
-import Button from "react-bootstrap/Button";
+import Footer from "./components/Footer";
+import NavBar from "./components/Nav";
 
 function App() {
   return (
     <>
-      <Button variant="primary">Primary</Button>{" "}
-      <Button variant="secondary">Secondary</Button>{" "}
-      <Button variant="success">Success</Button>{" "}
-      <Button variant="warning">Warning</Button>{" "}
-      <Button variant="danger">Danger</Button>{" "}
-      <Button variant="info">Info</Button>{" "}
-      <Button variant="light">Light</Button>{" "}
-      <Button variant="dark">Dark</Button>
-      <Button variant="link">Link</Button>
-      <div className="container-fluid">
-        <button className="btn btn-primary">Click Me</button>
+      <div className="">
+        <NavBar />
+        {/* <Footer /> */}
       </div>
     </>
   );
---
+++ src/components/Nav.jsx
@@ -0,0 +1,70 @@
+/* eslint-disable react/prop-types */
+import esewaLogo from "../images/esewa_logo.png";
+import searchIcon from "../images/searchIcon.svg";
+import lockIcon from "../images/lockIcon.svg";
+import userIcon from "../images/userIcon.svg";
+
+function NavBar() {
+  return (
+    <nav className="bg-primary px-md-3 px-2 pt-2 pt-md-3">
+      <div className="container-fluid d-flex flex-column row-gap-2 xlWidth">
+        <form className="row align-items-start justify-content-between">
+          <section className="col-4 align-self-center align-self-md-start">
+            <div className="row justify-content-between">
+              <div className="col col-sm-8 col-md-5">
+                <img src={esewaLogo} alt="logo" className="h-100 w-100" />
+              </div>
+
+              <SearchBox className="col-md-7 d-none d-md-flex" />
+            </div>
+          </section>
+
+          <section className="col-7">
+            <div className="row align-items-start justify-content-between">
+              <SearchBox
+                icon={userIcon}
+                placeholder="eSewa ID"
+                className="col-md-3 d-none d-md-flex"
+              />
+
+              <div className="col-md-4 d-none  d-md-block">
+                <SearchBox icon={lockIcon} placeholder="Password" />
+                <span className="text-light fs-6">Forgot Password?</span>
+              </div>
+              <button className="col-4 col-sm-2 offset-sm-10 offset-8 offset-md-0 col-md-2 btn btn-outline-light rounded-1 custom-btn">
+                Login
+              </button>
+              <button className="d-none d-md-block col-md-2 btn btn-outline-success rounded-1">
+                Register
+              </button>
+            </div>
+          </section>
+        </form>
+        <div className="row">
+          <SearchBox className="col d-md-none mb-2" />
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
+
+function SearchBox({
+  placeholder = "Search services/merchant by tags (e.g. adsl)",
+  icon = searchIcon,
+  className = "",
+}) {
+  return (
+    <div
+      className={`d-flex bg-dark align-items-center p-2 rounded-2 ${className}`}
+    >
+      <img src={icon} className="me-2" />
+      <input
+        type="text"
+        placeholder={placeholder}
+        className={`fs-6 bg-transparent border-0 w-100 h-100 text-light `}
+      />
+    </div>
+  );
+}
